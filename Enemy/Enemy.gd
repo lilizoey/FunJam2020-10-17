0ba811904114d60afb1928bafff61708307c4794
Lili Andersen <lili.andersen@nrk.no>
2020-10-17 22:23:15 +0200

    add roomba movement

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -7,9 +7,9 @@ export var floor_path: NodePath
 onready var _floor: Floor = get_node(floor_path)
 
 var next_point = null
-var move_speed: float = 120.0
-var move_distance: float = 160.0
+var move_speed: float = 40.0
 var resolved_hits: Dictionary = {}
+var move_direction = null
 
 var health: int = 500
 
@@ -18,47 +18,34 @@ func _ready():
 
 func _physics_process(delta):
 	resolved_hits = {}
-	if not next_point:
-		next_point = new_point()
+	if not move_direction:
+		move_direction = new_direction()
 	
-	var remainingVector: Vector2 = next_point - global_position
 	
-	if abs(remainingVector.y) > abs(remainingVector.x):
-		if remainingVector.y > 0:
+	if abs(move_direction.y) > abs(move_direction.x):
+		if move_direction.y > 0:
 			$AnimatedSprite.animation = "down"
 		else:
 			$AnimatedSprite.animation = "up"
 	else:
-		if remainingVector.x > 0:
+		if move_direction.x > 0:
 			$AnimatedSprite.animation = "right"
 		else:
 			$AnimatedSprite.animation = "left"
 	
-	var remainingLength = remainingVector.length()
+	move_and_slide(move_direction * move_speed)
 	
-	if is_on_wall() or remainingLength <= move_speed * delta * 2:
-		next_point = null
+	if is_on_wall():
+		move_direction = null
 		return
-	
-	var direction: Vector2 = remainingVector.normalized()
-	move_and_slide(direction * move_speed)
 	
 	for i in get_slide_count():
 		var collider = get_slide_collision(i).collider
 		hit(collider)
 
-func new_point() -> Vector2:
+func new_direction() -> Vector2:
 	var angle = rand_range(-PI, PI)
-	var direction = Vector2(cos(angle), sin(angle))
-	var point = direction * rand_range(0, move_distance)
-	
-	$Ray.cast_to = point
-	$Ray.force_raycast_update()
-	
-	if $Ray.get_collider():
-		return $Ray.get_collision_point()
-	else:
-		return global_position + point
+	return Vector2(cos(angle), sin(angle))
 
 
 func hit(obj: Object):

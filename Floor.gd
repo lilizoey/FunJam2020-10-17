6e0378a7fe5d89432452dab70d13b7a1353ce955
ITR <ITR@live.no>
2020-10-17 22:06:48 +0200

    Added method of knowing if a tile is clean or nah?

--- Floor.gd
+++ Floor.gd
@@ -3,12 +3,13 @@ extends Node2D
 
 var tiles: Array = []
 
+var min_x: int
+var min_y: int
+var max_x: int
+var max_y: int
+
 func _ready():
 	var cells = $Base.get_used_cells()
-	var min_x
-	var min_y
-	var max_x
-	var max_y
 	for cell in cells:
 		if not min_x or min_x > cell.x:
 			min_x = cell.x
@@ -20,16 +21,32 @@ func _ready():
 		if not max_y or max_y < cell.y:
 			max_y = cell.y
 	
-	for x in range(min_x, max_y):
-		tiles.append([])
-		for y in range(min_y, max_y):
-			tiles.append($Base.get_cell(x,y))
-			print($Base.get_cell(x,y))
-
+	for x in range(min_x, max_x + 1):
+		var row = []
+		tiles.append(row)
+		for y in range(min_y, max_y + 1):
+			row.append($Base.get_cell(x, y) != -1)
+	
+	for x in range(min_x - 5, max_x + 5):
+		for y in range(min_y - 5, max_y + 5):
+			is_clean(x, y)
 
 
 func _process(delta):
 	pass
 
 func clean(x: int, y: int):
+	if is_clean(x, y):
+		return
 	pass
+
+func is_clean(x: int, y: int) -> bool:
+	if x < min_x or x > max_x:
+		return true
+	if y < min_y or y > max_y:
+		return true
+
+	return tiles[x - min_x][y - min_y];
+
+func v_is_clean(position: Vector2) -> bool:
+	return is_clean(floor(position.x + 0.5), floor(position.y + 0.5))
